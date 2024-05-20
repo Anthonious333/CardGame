ecbb47b34feabb9fb411e3e0b9252315a2239b2a
Anthonious333 <Anthonious333@gmail.com>
2024-05-20 16:33:06 -0400

    andrew cookewd

--- CardGame/src/BigBoss/Modification.java
+++ CardGame/src/BigBoss/Modification.java
@@ -1,16 +1,44 @@
 package BigBoss;
 
+import java.util.ArrayList;
 
 public class Modification {
 
-	String name;
+	private String name;
+	private boolean unlockable;
 	private boolean unlocked;
+	private ArrayList<Modification> next = new ArrayList<Modification>();
+	private Modification last;
 	
 
-	public Modification (String name) {
+	public Modification (String name, Modification last, Modification... next) {
+		for (Modification m : next) {
+			this.next.add(m);
+		}
+		this.unlockable = true;
+		this.last = last;
 		this.name = name;
 		setUnlocked(false);
 	}
+	
+	
+	
+	public void setPath() {
+		for (Modification m : last.getNext()) {
+			if (!m.equals(this)) {
+				m.closePath();
+			}
+		}
+	}
+	
+	public void closePath() {
+		this.unlockable = false;
+		for(Modification m : next) {
+			m.closePath();
+		}
+	}
+	
+	
 	
 	@Override
 	public String toString () {
@@ -23,6 +51,14 @@ public class Modification {
 
 	public void setUnlocked(boolean unlocked) {
 		this.unlocked = unlocked;
+	}
+
+	public ArrayList<Modification> getNext() {
+		return next;
+	}
+
+	public void setNext(ArrayList<Modification> next) {
+		this.next = next;
 	}
 	
 }
